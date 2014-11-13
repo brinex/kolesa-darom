2cff4172c5618a58b6bdd39ef1271084ba6ca395
root <root@new.kolesa-darom.ru>
2014-11-13 09:53:12 +0300

    версия инициализации проекта

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=brinex',
+    'username' => 'brinex',
+    'password' => 'G90BqaKJ',
+    'charset' => 'utf8',
+];
